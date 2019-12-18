338655cfa6e41810436c9f7b2a38cdcba9e81196
Meet Usadadiya <meet123.usadadiya@gmail.com>
2019-12-18 11:38:17 +0530

    Delete BMI.java

--- BMI.java
+++
@@ -1,8 +0,0 @@
-class BMI {
-    public static void main(String args[]) {
-        double height = 63.2;
-        double weight = 200.1;
-        System.out.println("BMI is for height: " + height + " inches and weight: " + weight + " lbs is : "
-                + (weight / (height * height)));
-    }
-}
