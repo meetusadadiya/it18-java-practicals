22313da23a7815f55110b06805a3e7ad0be74cd1
Meet Usadadiya <meet123.usadadiya@gmail.com>
2019-12-18 11:35:58 +0530

    Add files via upload

---
+++ feet-metre.java
@@ -0,0 +1,7 @@
+class Feet_Metre {
+
+    public static void main(String args[]) {
+        double metre = 26.0;
+        System.out.println(metre + " metres are equal to " + 3.28 * metre + " feet");
+    }
+}
